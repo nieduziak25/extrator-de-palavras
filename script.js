4c7f37bbc02148e4a10dfeefe6bd035de7e8bdee
jose gustavo <jose.nieduziak@escola.pr.gov.br>
2025-09-23 09:40:41 -0300

    terminado aula 5

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-
 const botaoMostrapalavras = document.querySelector( "#botao-palavrachave");
 
 botaoMostrapalavras.addEventListener("click", mostraPalavrasChave);
 
 funtion mostrapalvrachave(){
     const texto = document.querySelector("#entrada-de-texto#").ariaValueMax;
-
     const camporesdultado = document.querySelector("#resultado-de-palavrachave");
-
     const palavrasChave = processaTexto(texto);
-
-    camporesdultado.textContent = palavras;
+   
+   '' camporesdultado.textContent = palavras;
 }
+
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+    
+let frequencia = {};
+for (let i of palvras) {
+    frequencia[1] =0;
+for (let j of palavras) {
+     if(i==j) {
+        frequencias[1]++;
+     }
+}
+    }
     return palavras;
 }
